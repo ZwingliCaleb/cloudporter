4db8d97e7b6e4086c5c03a45c29c3e5073baad2e
ZwingliCaleb <calebzwingli@gmail.com>
2025-01-05 22:56:55 +0300

    mod my dimensions

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -7,7 +7,7 @@ import FileList from '../components/FileList';
 
 const dashboard = () => {
   return (
-    <div className="rounded-lg shadow-lg w-full min-h-screen max-w-4xl mx-auto p-8 m-4">
+    <div className="rounded-lg shadow-lg w-full min-h-screen max-w-8xl mx-auto p-8 m-4">
       <div className = "min-h-screen bg-gray-100 flex flex-col items-center py-8">
       <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-4xl">
